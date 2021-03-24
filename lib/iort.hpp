8fed79d27913a723ade3867e56c0c084b0885dd0
PaperFanz <petefan.7@gmail.com>
2021-03-24 13:11:07 -0500

    initial commit

---
+++ lib/iort.hpp
@@ -0,0 +1,15 @@
+/*
+
+    iort.hpp - header file for the internet of robotic things compatibility 
+    layer for C++. This library wraps calls to the cloud API and provides a
+    simplified interface to IoT endpoints.
+
+    Dependencies:
+        - https://github.com/whoshuu/cpr (thank god no libcurl)
+        - https://github.com/open-source-parsers/jsoncpp
+
+*/
+
+// TODO function prototypes for blocking requests
+
+// TODO function prototypes for async requests
---
+++ src/iort.cpp
@@ -0,0 +1,5 @@
+/*
+
+    iort_interface.cpp
+
+*/
