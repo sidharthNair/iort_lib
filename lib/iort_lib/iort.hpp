dae100fc98b3d5932e1f3d03d08b83965e056bf9
PaperFanz <petefan.7@gmail.com>
2021-03-26 19:43:16 -0500

    minimally building headers

--- lib/iort_lib/iort.hpp
+++ lib/iort_lib/iort.hpp
@@ -9,7 +9,53 @@
         - https://github.com/open-source-parsers/jsoncpp
 
 */
+#ifndef iort_HPP
+#define iort_HPP
 
-// TODO function prototypes for blocking requests
+#include <json/json.h>
+#include <chrono>
 
-// TODO function prototypes for async requests
+using namespace std::chrono_literals;
+
+namespace iort {
+
+class Subscriber
+{
+private:
+    std::string uuid;
+    void (* cb)(Json::Value);
+
+public:
+    template<typename TDuration>
+    Subscriber(const std::string& uuid_, void (* cb_)(Json::Value &), 
+               TDuration timeout);
+
+    Subscriber(const std::string& uuid_, void (* cb_)(Json::Value &));
+
+    ~Subscriber();
+
+    bool isRunning(void);
+    void start(void);
+    void stop(void);
+};
+
+class Core
+{
+private:
+
+public:
+    Core();
+    ~Core();
+
+    template<typename TDuration>
+    Json::Value & get(const std::string& uuid_, TDuration timeout);
+
+    Json::Value & get(const std::string& uuid_);
+
+    Subscriber subscribe(const std::string& uuid_, void (* cb_)(Json::Value &));
+
+};
+
+} // end namespace iort
+
+#endif  // end iort_HPP
--- src/iort_lib/iort.cpp
+++ src/iort_lib/iort.cpp
@@ -3,3 +3,72 @@
     iort_interface.cpp
 
 */
+
+#include <cpr/cpr.h>
+#include "iort_lib/iort.hpp"
+
+namespace iort
+{
+
+template<typename TDuration>
+Subscriber::Subscriber(const std::string& uuid_, void (* cb_)(Json::Value &), 
+            TDuration timeout)
+{
+    // TODO
+}
+
+Subscriber::Subscriber(const std::string& uuid_, void (* cb_)(Json::Value &))
+{
+    // TODO
+}
+
+Subscriber::~Subscriber()
+{
+    // TODO
+}
+
+bool Subscriber::isRunning(void)
+{
+    // TODO
+}
+
+void Subscriber::start(void)
+{
+    // TODO
+}
+
+void Subscriber::stop(void)
+{
+    // TODO
+}
+
+Core::Core()
+{
+    // TODO
+}
+
+Core::~Core()
+{
+    // TODO
+}
+
+template<typename TDuration>
+Json::Value & Core::get(const std::string& uuid_, TDuration timeout)
+{
+    // TODO
+}
+
+Json::Value & Core::get(const std::string& uuid_)
+{
+    // TODO
+}
+
+Subscriber Core::subscribe(const std::string& uuid_, 
+                           void (* cb_)(Json::Value &))
+{
+    // TODO
+}
+
+    
+} // end namespace iort
+
