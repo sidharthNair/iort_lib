ee90d0267f361dc598ce732dc64ee7079ce45e96
Sidharth Nair <84476225+sidharthNair@users.noreply.github.com>
2021-07-23 14:01:26 -0500

    updated config.h with dummy links

--- inc/config.h
+++ inc/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define HTTP_ENDPOINT "https://5p1y6wnp3k.execute-api.us-west-2.amazonaws.com/default/iort_lib_query"
-#define MQTT_ENDPOINT "a22ztr6so9o7g8-ats.iot.us-west-2.amazonaws.com"
+#define HTTP_ENDPOINT "copy url here"
+#define MQTT_ENDPOINT "copy url here"
 
 #endif
